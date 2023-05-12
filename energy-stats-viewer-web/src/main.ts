ff4b223754b00fa3224e42fc053428d7a2b5655c
Olivier R. Bernier <orbernier@outlook.com>
2023-05-11 20:03:54 -0400

    add vuetify and config for it

--- energy-stats-viewer-web/src/main.ts
+++ energy-stats-viewer-web/src/main.ts
@@ -1,14 +1,17 @@
-import './assets/main.css';
-
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createVuetify } from 'vuetify';
 
 import App from './App.vue';
 import router from './router';
+import 'vuetify/styles';
+
+const vuetify = createVuetify();
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(vuetify);
 
 app.mount('#app');
