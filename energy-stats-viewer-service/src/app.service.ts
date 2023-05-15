bee2dbc76bd8daa985c62822e37a7d261c333ba2
Olivier R. Bernier <orbernier@outlook.com>
2023-05-14 21:10:21 -0400

    Add service to call fetch service and return data

--- energy-stats-viewer-service/src/app.service.ts
+++ energy-stats-viewer-service/src/app.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { FetchService } from './services/fetch.service';
+import { Demand } from './interfaces/demand';
+import { Production } from './interfaces/production';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly fetchService: FetchService) {}
+
+  async getDemandData(): Promise<Demand[]> {
+    const data = await this.fetchService.getDemandData();
+    const demandList: Demand[] = [];
+    data.forEach((demand) => {
+      demandList.push({
+        date: demand.date,
+        total: demand.valeurs.demandeTotal,
+      });
+    });
+    return demandList;
+  }
+
+  async getProductionDemand(): Promise<Production[]> {
+    const data = await this.fetchService.getProductionData();
+    const productionList: Production[] = [];
+    data.forEach((production) => {
+      productionList.push({
+        date: production.date,
+        total: production.valeurs.total,
+        hydraulic: production.valeurs.hydraulique,
+        wind: production.valeurs.eolien,
+        other: production.valeurs.autres,
+        solar: production.valeurs.solaire,
+        thermal: production.valeurs.thermique,
+      });
+    });
+    return productionList;
   }
 }
