0c6e919df890d594448336746b6236d1cb2e6bc9
Olivier R. Bernier <orbernier@outlook.com>
2023-05-17 05:16:02 -0400

    Create a grid view production page

---
+++ energy-stats-viewer-web/src/views/ProductionGridView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import ProductionCard from '@/components/production/ProductionCard.vue';
+</script>
+
+<template>
+  <div class="container">
+    <h1>Energy Production in Quebec Province by Type for each Date Time</h1>
+    <ProductionCard />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+.container h1 {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+</style>
