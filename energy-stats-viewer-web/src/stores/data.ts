2074172dc96f2c1dec61e2b2b36bc95fd8227313
Olivier R. Bernier <orbernier@outlook.com>
2023-05-17 05:08:05 -0400

    Add a data store to store data for the app

--- energy-stats-viewer-web/src/stores/data.ts
+++ energy-stats-viewer-web/src/stores/data.ts
@@ -14,4 +14,89 @@ export const useDataStore = defineStore('data', {
       productionList: [],
     };
   },
+  getters: {
+    getDemandList(state) {
+      return state.demandList.filter((demand) => demand.total);
+    },
+    getDemandDates(state) {
+      return state.demandList.map((demand) => demand.date);
+    },
+    getTotalDemand(state) {
+      let total = 0;
+      state.demandList.forEach((demand) => {
+        demand.total ? (total += demand.total) : total;
+      });
+      return total;
+    },
+    getTotalDemandByDate(state) {
+      return state.demandList.map((demand) => demand.total);
+    },
+    getDemandByProductionDate(): number[] {
+      const prodDates = this.getProductionDates;
+      const demandList = this.getDemandList;
+
+      const data: number[] = [];
+      demandList.forEach((demand) => {
+        prodDates.forEach((prod) => {
+          if (demand.date === prod) {
+            data.push(demand.total || 0);
+          }
+        });
+      });
+      return data;
+    },
+    getProductionList(state) {
+      return state.productionList.filter(
+        (production) => production.total !== 0,
+      );
+    },
+    getProductionDates(): string[] {
+      const prodList = this.getProductionList;
+      const data = prodList.map((prod) => prod.date);
+      return data;
+    },
+    getTotalProduction(state) {
+      let total = 0;
+      state.productionList.forEach((prod) => {
+        total += prod.total;
+      });
+      return total;
+    },
+    getTotalProductionByDate(state) {
+      return state.productionList.map((prod) => prod.total);
+    },
+    getTotalProductionByTypes(state) {
+      const computedData = {
+        totalHydraulic: 0,
+        totalWind: 0,
+        totalSolar: 0,
+        totalThermal: 0,
+        totalOthers: 0,
+      };
+      state.productionList.forEach((prod) => {
+        prod.hydraulic
+          ? (computedData.totalHydraulic += prod.hydraulic)
+          : computedData.totalHydraulic;
+        prod.wind
+          ? (computedData.totalWind += prod.wind)
+          : computedData.totalWind;
+        prod.solar
+          ? (computedData.totalSolar += prod.solar)
+          : computedData.totalSolar;
+        prod.thermal
+          ? (computedData.totalThermal += prod.thermal)
+          : computedData.totalThermal;
+        prod.other
+          ? (computedData.totalOthers += prod.other)
+          : computedData.totalOthers;
+      });
+      return [
+        computedData.totalHydraulic,
+        computedData.totalWind,
+        computedData.totalSolar,
+        computedData.totalThermal,
+        computedData.totalOthers,
+      ];
+    },
+  },
 });
