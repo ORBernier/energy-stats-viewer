dcac92801a8f6230d91308c17e26b2b37f5bb50e
Olivier R. Bernier <orbernier@outlook.com>
2023-05-17 05:17:55 -0400

    Update demand page to use the chart and data table

--- energy-stats-viewer-web/src/views/DemandListView.vue
+++ energy-stats-viewer-web/src/views/DemandListView.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DemandTable from '../components/demand/DemandTable.vue';
+import DemandLineChart from '@/components/demand/DemandLineChart.vue';
+</script>
 
 <template>
-  <h1>DEMAND LIST</h1>
+  <div class="container">
+    <h1>Total Energy Demand in Quebec Province</h1>
+    <DemandLineChart />
+    <DemandTable />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+.container h1 {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+</style>
