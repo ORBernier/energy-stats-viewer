447de785bf475cf14751b13411ebed1925d59653
Olivier R. Bernier <orbernier@outlook.com>
2023-05-17 04:44:43 -0400

    fix backend syntax for the controllers

--- energy-stats-viewer-service/src/production/production.controller.ts
+++ energy-stats-viewer-service/src/production/production.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { Production } from 'src/interfaces/production';
+import { Controller, Get, Res } from '@nestjs/common';
 import { ProductionService } from './production.service';
+import { Response } from 'express';
 
 @Controller('production')
 export class ProductionController {
   constructor(private readonly productionService: ProductionService) {}
 
   @Get('/data')
-  async getData(): Promise<Production[]> {
-    return await this.productionService.getProductionData();
+  async getData(@Res() res: Response) {
+    const data = await this.productionService.getProductionData();
+    res.set({ 'Access-Control-Allow-Origin': ['*'] }).send(data);
   }
 }
