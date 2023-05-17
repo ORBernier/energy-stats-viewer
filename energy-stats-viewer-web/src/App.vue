ddf257265fde3cac5aeea2c124bbbacace3f212d
Olivier R. Bernier <orbernier@outlook.com>
2023-05-17 05:10:01 -0400

    Add calls to fetch data then update store

--- energy-stats-viewer-web/src/App.vue
+++ energy-stats-viewer-web/src/App.vue
@@ -1,6 +1,25 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import NavBar from './components/NavBar.vue';
+import { useDataStore } from './stores/data';
+import {
+  fetchDemandData,
+  fetchProductionData,
+} from './shared/services/dataService';
+
+const dataStore = useDataStore();
+
+fetchDemandData().then((data) => {
+  dataStore.$patch({
+    demandList: data,
+  });
+});
+
+fetchProductionData().then((data) => {
+  dataStore.$patch({
+    productionList: data,
+  });
+});
 </script>
 
 <template>
@@ -14,6 +33,9 @@ import NavBar from './components/NavBar.vue';
 
 <style scoped>
 body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   margin-left: 56px;
   padding-left: 1rem;
   background-color: whitesmoke;
