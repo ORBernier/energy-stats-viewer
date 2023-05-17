9a41ea6aa98b6930d8b8c8ab08a0bff10ca8f504
Olivier R. Bernier <orbernier@outlook.com>
2023-05-17 05:13:56 -0400

    Create a dashboard page with the line chart

---
+++ energy-stats-viewer-web/src/views/DashboardView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import DashboardLineChart from '@/components/DashboardLineChart.vue';
+</script>
+
+<template>
+  <div class="container">
+    <h1>DASHBOARD</h1>
+    <DashboardLineChart />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+.container h1 {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+</style>
